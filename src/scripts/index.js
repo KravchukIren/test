44974b7356797e4fa51b9bcb74500b98fe2b0470
KravchukIren <Iryna.Kravchuk61@gmail.com>
2020-02-29 03:19:27 +0200

    JavaScript first part

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,44 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
 
-console.log('webpack starterkit');
+const a = 8;
+console.log("const = ", a);
+
+let b = 44;
+console.log("Let = ", b);
+
+const sum = a + b;
+console.log("сумма = ", sum);
+
+const razn = a - b;
+console.log("разница = ", razn);
+
+const pro = a * b;
+console.log("произведение = ", pro);
+
+const chas = a / b;
+console.log("частное = ", chas);
+
+const lastname = "Petrov";
+console.log(lastname);
+const lastName = "Ivanov";
+console.log(lastName);
+
+console.log("camelCase");
+// console.log("PascalCase");
+// console.log("kebab-case");
+// console.log("snake_case");
+
+// var c = 16;
+/* console.log("Var = ", c); */
+
+// console.log("Desktop => Electron");
+// console.log("Smartphone & Tablet => Cordova");
+// console.log("Server => Node.js");
+// console.log("---History---");
+// console.info("1995 - Netscape");
+// console.info("1997 - ECMA Strardt");
+// console.log("Ecma Script = JavaScript");
+// console.log("1999 - EcmaScript 3");
+// console.log("2009 - EcmaScript 5 ==> ES5");
+// console.log("2015 - EcmaScript 2015 ==> ES6");
+// console.log(">2015 - new release");
